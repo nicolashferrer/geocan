fb5655a69a19e5f29bbd15c5520fe5118e49db24
nicolas.anibal.martinez <nicolas.anibal.martinez@gmail.com>
2012-04-30 19:27:28 +0000

    cambios en beforeSave

--- app/Model/Patient.php
+++ app/Model/Patient.php
@@ -102,11 +102,13 @@ class Patient extends AppModel {
             {
                 $this->data['Patient']['fecha_nacimiento'] = implode('-', array_reverse(explode('/', $this->data['Patient']['fecha_nacimiento'])));
             }
-            if (!empty($this->data['Patient']['nro_documento']))
-            {
-                $documentoEncriptado = Security::hash($this->data['Patient']['nro_documento'], 'sha256', true);
-                $this->data['Patient']['nro_documento'] = $documentoEncriptado;
-            }
+			if (empty($this->data['Patient']['id'])) { // Si es una alta de nuevo paciente...
+				if (!empty($this->data['Patient']['nro_documento']))
+				{
+					$documentoEncriptado = Security::hash($this->data['Patient']['nro_documento'], 'sha256', true);
+					$this->data['Patient']['nro_documento'] = $documentoEncriptado;
+				}
+			}
             return true;
         }
 		
