c0ea4333a0d0f07f359263c09f87c1d43f5268fd
nicolas.anibal.martinez@gmail.com <nicolas.anibal.martinez@gmail.com@36157a8b-d053-33d6-2e79-2abcae19b089>
2012-08-28 03:50:37 +0000

    Nueva tabla de empleos!

---
+++ trunk/app/Model/Job.php
@@ -0,0 +1,51 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Job Model
+ *
+ * @property Patient $Patient
+ * @property Test $Test
+ */
+class Job extends AppModel {
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'descripcion' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Patient' => array(
+			'className' => 'Patient',
+			'foreignKey' => 'job_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
